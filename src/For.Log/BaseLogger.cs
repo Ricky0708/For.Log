10e1bf4f710e40218739378930da433f16e1c84d
Ricky <Ricky@DESKTOP-AH6THCK>
2017-01-20 17:05:10 +0800

    加入L

--- src/For.Log/BaseLogger.cs
+++ src/For.Log/BaseLogger.cs
@@ -10,7 +10,14 @@ namespace For.Log
     public abstract class BaseLogger
     {
 
-        public LoggerProperty LoggerProperty { get; set; }
+        private LoggerProperty _loggerProperty;
+
+
+        public BaseLogger(LoggerProperty LoggerProperty)
+        {
+            _loggerProperty = LoggerProperty;
+        }
+
         private object lockObject = new object();
 
         public virtual void WriteLogAtEndRequest(string log)
@@ -20,59 +27,58 @@ namespace For.Log
 
         public void Fatal(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Fatal) > 0)
+            if ((_loggerProperty.Level & LogLevel.Fatal) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => WriteFatalAsync(log, stackTrace, envStackTrace));
+                WriteLog(() => WriteFatalAsync(log, GetLogInfo()));
             }
         }
+
         public void Error(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Error) > 0)
+            if ((_loggerProperty.Level & LogLevel.Error) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => WriteErrorAsync(log, stackTrace, envStackTrace));
+                WriteLog(() => WriteErrorAsync(log, GetLogInfo()));
             }
         }
+
         public void Warn(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Warn) > 0)
+            if ((_loggerProperty.Level & LogLevel.Warn) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => WriteWarnAsync(log, stackTrace, envStackTrace));
+                WriteLog(() => WriteWarnAsync(log, GetLogInfo()));
             }
         }
         public void Info(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Info) > 0)
+            if ((_loggerProperty.Level & LogLevel.Info) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => WriteInfoAsync(log, stackTrace, envStackTrace));
+                WriteLog(() => WriteInfoAsync(log, GetLogInfo()));
             }
         }
         public void Debug(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Debug) > 0)
+            if ((_loggerProperty.Level & LogLevel.Debug) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => { WriteDebugAsync(log, stackTrace, envStackTrace); });
+                WriteLog(() => WriteDebugAsync(log, GetLogInfo()));
             }
         }
         public void Trace(string log)
         {
-            if ((LoggerProperty.Level & LogLevel.Trace) > 0)
+            if ((_loggerProperty.Level & LogLevel.Trace) > 0)
             {
-                var stackTrace = new System.Diagnostics.StackTrace();
-                var envStackTrace = Environment.StackTrace;
-                WriteLog(() => WriteTraceAsync(log, stackTrace,envStackTrace));
+                WriteLog(() => WriteTraceAsync(log, GetLogInfo()));
             }
         }
 
+        private LogInfo GetLogInfo()
+        {
+            return new LogInfo()
+            {
+                LogTime = DateTime.Now,
+                envStackTrace = Environment.StackTrace,
+                stackTrace = new System.Diagnostics.StackTrace()
+            };
+        }
         private void WriteLog(Action write)
         {
             System.Threading.Thread th = new System.Threading.Thread(() =>
@@ -85,11 +91,11 @@ namespace For.Log
             th.Start();
         }
 
-        protected abstract void WriteFatalAsync(string log, StackTrace stackTrace, string envStackTrace);
-        protected abstract void WriteErrorAsync(string log, StackTrace stackTrace, string envStackTrace);
-        protected abstract void WriteWarnAsync(string log, StackTrace stackTrace, string envStackTrace);
-        protected abstract void WriteInfoAsync(string log, StackTrace stackTrace, string envStackTrace);
-        protected abstract void WriteDebugAsync(string log, StackTrace stackTrace, string envStackTrace);
-        protected abstract void WriteTraceAsync(string log, StackTrace stackTrace, string envStackTrace);
+        protected abstract void WriteFatalAsync(string log, LogInfo logInfo);
+        protected abstract void WriteErrorAsync(string log, LogInfo logInfo);
+        protected abstract void WriteWarnAsync(string log, LogInfo logInfo);
+        protected abstract void WriteInfoAsync(string log, LogInfo logInfo);
+        protected abstract void WriteDebugAsync(string log, LogInfo logInfo);
+        protected abstract void WriteTraceAsync(string log, LogInfo logInfo);
     }
 }
