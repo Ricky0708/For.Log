bf8604f98d8af02effad98b5c8cb70a89c4d08f0
Ricky0708 <Ricky0708@live.com>
2017-01-20 17:11:03 +0800

    Add files via upload

---
+++ src/For.Log/LogInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For.Log
+{
+    public class LogInfo
+    {
+        public DateTime LogTime { get; set; }  
+        public StackTrace stackTrace { get; set; }
+        public string envStackTrace { get; set; }
+        public int test { get; set; }
+
+    }
+}
